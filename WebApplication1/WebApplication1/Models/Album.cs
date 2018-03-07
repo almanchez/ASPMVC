70ce31fec3ced2df213f722862645a40a69250b5
lalmanza <lalmanza@P-SIS-DES-193>
2018-03-07 11:15:49 -0500

    add album/astist classes

---
+++ WebApplication1/WebApplication1/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+
+        public int Title { get; set; }
+
+        public int Artist { get; set; }
+
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Artista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Artista
+    {
+        public int ArtistID { get; set; }
+
+        public String Name { get; set; }
+
+        public  List <Album> Albums { get; set; }
+
+    }
+}
